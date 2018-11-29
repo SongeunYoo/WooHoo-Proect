725fff51214fdbe46842ae31f78ff86ac6df44e0
oversteer911 <44688051+oversteer911@users.noreply.github.com>
2018-11-30 04:11:43 +0900

    Update layout.c

--- Shin/layout.c
+++ Shin/layout.c
@@ -1,4 +1,3 @@
-#define _XOPEN_SOURCE 500
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,17 +10,13 @@
 
 #define SIZE 4
 uint32_t score=0;
-uint8_t scheme=0;
 
 void getColor(uint8_t value, char *color, size_t length) {
     uint8_t original[] = {8,255,1,255,2,255,3,255,4,255,5,255,6,255,7,255,9,0,10,0,11,0,12,0,13,0,14,0,255,0,255,0};
-    uint8_t blackwhite[] = {232,255,234,255,236,255,238,255,240,255,242,255,244,255,246,0,248,0,249,0,250,0,251,0,252,0,253,0,254,0,255,0};
-    uint8_t bluered[] = {235,255,63,255,57,255,93,255,129,255,165,255,201,255,200,255,199,255,198,255,197,255,196,255,196,255,196,255,196,255,196,255};
-    uint8_t *schemes[] = {original,blackwhite,bluered};
-    uint8_t *background = schemes[scheme]+0;
-    uint8_t *foreground = schemes[scheme]+1;
+    uint8_t *background = original+0;
+    uint8_t *foreground = original+1;
     if (value > 0) while (value--) {
-        if (background+2<schemes[scheme]+sizeof(original)) {
+        if (background+2<original+sizeof(original)) {
             background+=2;
             foreground+=2;
         }
@@ -70,8 +65,81 @@ void drawBoard(uint8_t board[SIZE][SIZE]) {
     printf("\n");
 }
 
-void main(int argc, char *argv[]) {
+void setBufferedInput(bool enable) {
+    static bool enabled = true;
+    static struct termios old;
+    struct termios new;
+    
+    if (enable && !enabled) {
+        // restore the former settings
+        tcsetattr(STDIN_FILENO,TCSANOW,&old);
+        // set the new state
+        enabled = true;
+    } else if (!enable && enabled) {
+        // get the terminal settings for standard input
+        tcgetattr(STDIN_FILENO,&new);
+        // we want to keep the old setting to restore them at the end
+        old = new;
+        // disable canonical mode (buffered i/o) and local echo
+        new.c_lflag &=(~ICANON & ~ECHO);
+        // set the new settings immediately
+        tcsetattr(STDIN_FILENO,TCSANOW,&new);
+        // set the new state
+        enabled = false;
+    }
+}
+
+void addRandom(uint8_t board[SIZE][SIZE]) {
+    static bool initialized = false;
+    uint8_t x,y;
+    uint8_t r,len=0;
+    uint8_t n,list[SIZE*SIZE][2];
+    
+    if (!initialized) {
+        srand(time(NULL));
+        initialized = true;
+    }
+    
+    for (x=0;x<SIZE;x++) {
+        for (y=0;y<SIZE;y++) {
+            if (board[x][y]==0) {
+                list[len][0]=x;
+                list[len][1]=y;
+                len++;
+            }
+        }
+    }
+    
+    if (len>0) {
+        r = rand()%len;
+        x = list[r][0];
+        y = list[r][1];
+        n = (rand()%10)/9+1;
+        board[x][y]=n;
+    }
+}
+
+void initBoard(uint8_t board[SIZE][SIZE]) {
+    uint8_t x,y;
+    for (x=0;x<SIZE;x++) {
+        for (y=0;y<SIZE;y++) {
+            board[x][y]=0;
+        }
+    }
+    addRandom(board);
+    addRandom(board);
+    drawBoard(board);
+    score = 0;
+}
+
+
+int main(int argc, char *argv[]) {
     uint8_t board[SIZE][SIZE];
+    
+    //initBoard(board);
+    signal(SIGINT, signal_callback_handler);
+    setBufferedInput(false);
     drawBoard(board);
     
+    return 1;
 }
