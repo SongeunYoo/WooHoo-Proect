642df67d0c6223a9fcccb76e2f0e6e68d68608a5
oversteer911 <44688051+oversteer911@users.noreply.github.com>
2018-11-29 15:54:13 +0900

    Update ranking.c

--- Shin/ranking.c
+++ Shin/ranking.c
@@ -1,20 +1,22 @@
 #include	<stdio.h>
-#include	<unistd.h>	/* for execlp */
-#include	<stdlib.h>	/* for exit   */
+#include	<unistd.h>
+#include	<stdlib.h>
 #include	<fcntl.h>
 #include	<sys/wait.h>
 
 int lowest_score = 0;
+int score = 0;
 
 int main()
 {
-    rank(2048);
+    if( score > lowest_score)//refresh ranklist
+        rank();
 }
 
-int rank(int score){
+int rank(){
     int fd;
     
     fd = open("ranklist", O_CREAT | O_WRONLY | O_APPEND, 0644);	/* then open */
-    write(fd, score, 12);
-
+    printf("%d\n",score);
+    write(fd,score,300);
 }
