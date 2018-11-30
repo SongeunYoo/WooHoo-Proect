9950643db6332a3f93910b1bf678a1e3d2fe0ebe
oversteer911 <44688051+oversteer911@users.noreply.github.com>
2018-12-01 03:34:04 +0900

    Update ranking.c

--- Shin/ranking.c
+++ Shin/ranking.c
@@ -1,22 +1,34 @@
+
+//시작 선택화면 / 랭킹 파일 Handler
+
 #include	<stdio.h>
 #include	<unistd.h>
 #include	<stdlib.h>
-#include	<fcntl.h>
-#include	<sys/wait.h>
+#defien MAX 4*10
 
 int lowest_score = 0;
 int score = 0;
+char username[MAX];
 
 int main()
 {
+    printf("Enter Your Name : ");
+    scanf("%s",&username);
+    
     if( score > lowest_score)//refresh ranklist
         rank();
+    
+    //GAME OVER
+    //SORT
+    //PRINT
+    
 }
 
 int rank(){
     int fd;
     
     fd = open("ranklist", O_CREAT | O_WRONLY | O_APPEND, 0644);	/* then open */
-    printf("%d\n",score);
+    
+    write(fd,username,300);
     write(fd,score,300);
 }
